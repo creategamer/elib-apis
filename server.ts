614a2be86776b2341d6bc07df93558b1952c21d0
creategamer <creategamer10@gmail.com>
2024-11-12 05:07:50 +0530

    add database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
+import connectDB from "./src/config/db";
 
-const startServer=()=>{
-    const port=process.env.PORT||3000
+const startServer=async()=>{
+    //connect to database
+    await connectDB()
+    const port=config.port||3000    
 
     app.listen(port,()=>{
         console.log(`listening on port:${port}`)
---
+++ src/config/db.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import { config } from './config'
+
+
+const connectDB=async()=>{
+    try {
+        mongoose.connection.on('connected',()=>{
+            console.log('Connected to database successfully')
+        })
+
+        mongoose.connection.on('error',(err)=>{
+            console.log('Error in connecting to database',err)
+        })
+        await mongoose.connect(config.databaseUrl as string)
+        
+    } catch (error) {
+        console.error('failed to connect database ',error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
