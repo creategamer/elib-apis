02b1be138b26458c12db86fcd2c76fb20cddbe87
creategamer <creategamer10@gmail.com>
2024-11-13 18:34:25 +0530

    upload files to cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,11 @@ const _config={
     port:process.env.PORT,
     databaseUrl:process.env.MONGO_CONNECTION_STRING,
     env:process.env.NODE_ENV,
-    jwtSecret:process.env.JWT_SECRET
+    jwtSecret:process.env.JWT_SECRET,
+    coudinary_cloud:process.env.CLOUDINARY_CLOUD,
+    coudinaryApikey:process.env.CLOUDINARY_API_KEY,
+    coudinaryApiSecret:process.env.CLOUDINARY_API_SECRET,
+    
 };
 
 
