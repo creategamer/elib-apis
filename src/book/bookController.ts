d05cc8a4b124e7b0530898dd67d27188782bacbe
creategamer <creategamer10@gmail.com>
2024-11-14 00:17:09 +0530

    add author id to new book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
 import fs from 'node:fs'
+import { AuthRequest } from "../middlewares/authentication";
 
 const createBook=async(req:Request,res:Response,next:NextFunction)=>{
     
@@ -40,13 +41,12 @@ const createBook=async(req:Request,res:Response,next:NextFunction)=>{
         // console.log('bookFileUploadResult:',bookFileUploadResult)
         // console.log('uploaded result:',uploadResult);
 
-        // @ts-ignore
-        console.log('userId:',req.userId)
+        const _req=req as AuthRequest
 
         const newBook=await bookModel.create({
             title,
             genre,
-            author:'6733a8d58d24f9b2011fa2db',
+            author:_req.userId,
             coverImage:uploadResult.secure_url,
             file:bookFileUploadResult.secure_url
         })
